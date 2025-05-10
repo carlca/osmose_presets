c9dfdd0979704b01789787e3db51a68d0e74be34
Carl Caulkett <carl.caulkett@gmail.com>
2025-05-10 14:39:32 +0100

    More refactoring.

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,21 +1,20 @@
 import mido
 from consts import *
 
-
 def get_input_ports():
   try:
     return mido.get_input_names()
   except Exception as e:
-    return [str(e)]
-
+    print(f"Error getting MIDI input ports: {e}")
+    return[]
 
 def get_longest_port_width():
   ports = get_input_ports()
   if DEBUG_LAYOUT:
     ports.append(TEXT_26)
     ports.append(TEXT_50)
-  max = 0
+  max_len = 0
   for port in ports:
-    if len(port) > max:
-      max = len(port)
-  return max
+    if len(port) > max_len:
+      max_len = len(port)
+  return max_len
