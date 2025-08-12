32a15b50a1d16873838068da8dc1c7dd33f265fe
Carl Caulkett <carl.caulkett@gmail.com>
2025-08-12 23:54:49 +0100

    FilterSelector events are working.

--- filter_selector.py
+++ filter_selector.py
@@ -9,7 +9,7 @@ class FilterSelector(Vertical):
    def __init__(self, filter, **kwargs):
       super().__init__(**kwargs)
       self.filter = filter
-      self._updating = False
+      # NO _updating flag is needed in this new version.
 
    def compose(self) -> ComposeResult:
       filter_name = "pack" if self.filter == Filters.PACK else "type"
@@ -21,40 +21,24 @@ class FilterSelector(Vertical):
          yield Checkbox(f_name, id=safe_id, classes="compact bold-text")
 
    def get_other_checkboxes(self) -> list[Checkbox]:
+      """Gets all checkboxes in this group except for the 'all' checkbox."""
       return [cb for cb in self.query(Checkbox) if cb.id != "check_all"]
 
    def all_checkbox_changed(self, event: Checkbox.Changed) -> None:
-      new_value = event.value
-      try:
-         self._updating = True
-         other_checkboxes = self.get_other_checkboxes()
+      all_box_value = event.value
+      other_checkboxes = self.get_other_checkboxes()
+      if all_box_value != all(cb.value for cb in other_checkboxes):
          for checkbox in other_checkboxes:
-            checkbox.value = new_value
-      finally:
-         self._updating = False
+            checkbox.value = all_box_value
 
    def other_checkbox_changed(self, event: Checkbox.Changed) -> None:
       all_box = self.query_one("#check_all", Checkbox)
-      if not event.value:
-         try:
-            self._updating = True
-            all_box.value = False
-         finally:
-            self._updating = False
-      else:
-         other_checkboxes = self.get_other_checkboxes()
-         all_checked = all(cb.value for cb in other_checkboxes)
-         try:
-            self._updating = True
-            all_box.value = all_checked
-         finally:
-            self._updating = False
+      all_are_checked = all(cb.value for cb in self.get_other_checkboxes())
+      if all_box.value != all_are_checked:
+         all_box.value = all_are_checked
 
    def on_checkbox_changed(self, event: Checkbox.Changed) -> None:
-      """Called when the value of any checkbox changes."""
-      if self._updating:
-         return
-      # Logic for the 'all' checkbox
+      """ a state-aware handler that prevents chain reactions without locks """
       if event.checkbox.id == "check_all":
          self.all_checkbox_changed(event)
       else:
