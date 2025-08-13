4458777bce418bd4f7f0eed1a395e987740c9a4d
Carl Caulkett <carl.caulkett@gmail.com>
2025-08-13 23:53:56 +0100

    Getting messages up to date.

--- messages.py
+++ messages.py
@@ -1,10 +1,9 @@
 from textual.message import Message
 from textual.widget import Widget
-
+from filters import Filters
 
 class FocusNextContainer(Message):
    """Message to signal that focus should move to the next container."""
-
    def __init__(self, sender: Widget) -> None:
       self.sender = sender
       super().__init__()
@@ -12,7 +11,13 @@ class FocusNextContainer(Message):
 
 class FocusPreviousContainer(Message):
    """Message to signal that focus should move to the previous container."""
-
    def __init__(self, sender: Widget) -> None:
       self.sender = sender
       super().__init__()
+
+class FilterChanged(Message):
+    """Posted when the selection in a FilterSelector changes."""
+    def __init__(self, filter_type: "Filters", selected_filters: list[str]) -> None:
+        self.filter_type = filter_type
+        self.selected_filters = selected_filters
+        super().__init__()
