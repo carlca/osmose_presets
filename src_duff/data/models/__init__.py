01333cf9974a674f2937367541f43c02159a1456
Carl Caulkett <carl.caulkett@gmail.com>
2025-09-28 13:54:31 +0100

    Deleted src_duff.

--- src_duff/data/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Data models for the Osmose Presets application.
-
-This module contains the core domain models used throughout the application.
-"""
-
-from .preset import Preset, PresetType
-
-__all__ = ['Preset', 'PresetType']
