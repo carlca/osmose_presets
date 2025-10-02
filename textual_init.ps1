ebbe1f3c5db590376593359cce0a6dc66e1c50e6
Carl Caulkett <carl.caulkett@gmail.com>
2025-10-02 14:59:11 +0100

    Tidied textual_init.ps1.

--- textual_init.ps1
+++ textual_init.ps1
@@ -1,20 +1,3 @@
-# if a venv is active, deativate it
-if ($env:VIRTUAL_ENV) {
-   .\.venv\Scripts\deactivate.bat
-}
-
-# if a venv folder is preset, the delete it
-if (Test-Path .venv) {
-   rm -r ./.venv
-}
-
-#
-python -m venv .venv
-
-if (-not $env:VIRTUAL_ENV) {
-   .\.venv\Scripts\Activate.ps1
-}
-
 pip install textual
 pip install textual-dev
 pip install "textual[syntax]"
