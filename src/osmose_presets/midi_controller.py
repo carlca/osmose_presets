e75db2c17e74c009084b964cca2c52e760b803df
Carl Caulkett <carl.caulkett@gmail.com>
2025-10-03 14:49:35 +0100

    Now up to 1 second delay.

--- src/osmose_presets/midi_controller.py
+++ src/osmose_presets/midi_controller.py
@@ -15,7 +15,7 @@ class MidiController:
 
       cc_msg = mido.Message("control_change", channel=0, control=0, value=cc)
       log(f"Sending CC message: {cc_msg.hex()}")
-      time.sleep(0.4)
+      time.sleep(1.0)
       output.send(cc_msg)
       log(f"Sent CC message: {cc_msg.hex()}")
 
