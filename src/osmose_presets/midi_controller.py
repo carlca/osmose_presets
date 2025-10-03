1a2ce1ac92b2fe69d09c50eeab6cc469b9521938
Carl Caulkett <carl.caulkett@gmail.com>
2025-10-03 13:56:59 +0100

    0.4 ???.

--- src/osmose_presets/midi_controller.py
+++ src/osmose_presets/midi_controller.py
@@ -15,7 +15,7 @@ class MidiController:
 
       cc_msg = mido.Message("control_change", channel=0, control=0, value=cc)
       log(f"Sending CC message: {cc_msg.hex()}")
-      time.sleep(0.2)
+      time.sleep(0.4)
       output.send(cc_msg)
       log(f"Sent CC message: {cc_msg.hex()}")
 
