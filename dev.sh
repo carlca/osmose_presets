71eca26992dd5f5a56a966d9e0bcf48fcce69931
Carl Caulkett <carl.caulkett@gmail.com>
2026-09-24 22:52:57 +0100

    Upgraded to Python 3.14.4 + UV.

--- dev.sh
+++ dev.sh
@@ -1,2 +1,6 @@
 #!/bin/zsh
-watchfiles "textual run --dev src/osmose_presets/app.py" ./src/osmose_presets/*.py ./src/osmose_presets/*.tcss
+
+uv run watchfiles \
+  "textual run --dev src/osmose_presets/app.py" \
+  ./src/osmose_presets/*.py \
+  ./src/osmose_presets/*.tcss
